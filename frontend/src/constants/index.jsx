7830e3044242e8101cfe6bd20372028da5744755
Akshi Takle <akshi.takle17@gmail.com>
2026-02-16 20:09:18 +0530

    Project Completed

--- frontend/src/constants/index.jsx
+++
@@ -1,7 +0,0 @@
-export const navItems = [
-  { label: "Home", href: "/" },
-  { label: "Explore", href: "/explore" },
-  { label: "Studio", href: "/studio" },
-  { label: "Analytics", href: "/analytics" },
-  { label: "Profile", href: "/profile" },
-];
--- frontend/src/lib/ContractAddress.jsx
+++
@@ -1,3 +0,0 @@
-const contractAddress = "0x2C5B02196082Bb7eAe7c1a13095558834A659689";
-
-export default contractAddress;
